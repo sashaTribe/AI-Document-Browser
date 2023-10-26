de0ce105daba9e732965fc6226016debe74f4fde
Ricardo <ricardo.lora@xandertalent.com>
2023-10-26 13:02:49 +0100

    Working AI

--- app.py
+++ app.py
@@ -7,7 +7,14 @@ from langchain.vectorstores.faiss import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
-
+from langchain.schema import (
+    SystemMessage,
+    HumanMessage,
+    AIMessage
+)
+from streamlit_chat import message
+import os
+ 
 def get_pdf_text(pdf_docs):
     text = ""
     for pdf in pdf_docs:
@@ -41,15 +48,42 @@ def get_conversation_chain(vectorstore):
     )
     return conversation_chain
 
-def main():
+def init():
     load_dotenv()
+    # test that the API key exists
+    if os.getenv("OPENAI_API_KEY") is None or os.getenv("OPENAI_API_KEY") == "":
+        print("OPENAI_API_KEY is not set")
+        exit(1)
+    else:
+        print("OPENAI_API_KEY is set")
+
     st.set_page_config(page_title="AI Document Reader Chat", page_icon=":books:")
 
-    if "conversation_chain" not in st.session_state:
-        st.session_state.conversation_chain = None
+def main():
+    init()
+
+    chat = ChatOpenAI(temperature=1)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = [
+            SystemMessage(content="Hello there! How may I be of assistance?")
+            ]
 
     st.header("AI Document Reader Chat")
-    st.text_input("Ask a question about your document")
+
+    user_question = st.text_input("Ask a question about your document: ", key="user_question")
+    if user_question and user_question not in [message.content for message in st.session_state.conversation]:
+       st.session_state.conversation.append(HumanMessage(content=user_question))
+       with st.spinner("Thinking..."):
+            response = chat(st.session_state.conversation)
+       st.session_state.conversation.append(AIMessage(content=response.content))
+    
+    conversation = st.session_state.get('conversation', [])
+    for i, msg in enumerate(conversation[1:]):
+        if i % 2 == 0:
+            message(msg.content, is_user=True, key=str(i) + '_user')
+        else:
+            message(msg.content, is_user=False, key=str(i) + '_ai')
 
     with st.sidebar:
         st.subheader("Your Documents")
@@ -57,11 +91,10 @@ def main():
             "Upload your data and click on Process", accept_multiple_files=True)
         if st.button("Process"):
             with st.spinner("Processing"):
-                raw_text = get_pdf_text(pdf_docs)                                           # Fetch PDF
-                text_chunks = get_text_chunks(raw_text)                                     # Turn PDF in chunks
+                raw_text = get_pdf_text(pdf_docs)                                           # Turns PDF Into Text
+                text_chunks = get_text_chunks(raw_text)                                     # Splits Text in chunks
                 vectorstore = get_vectorstore(text_chunks)                                  # Store Vectors
-                st.session_state.coversation_chain = get_conversation_chain(vectorstore)    # Conversation Chain
-    st.session_state.coversation_chain
+                st.session_state.conversation = get_conversation_chain(vectorstore)    # Conversation Chain
 
 if __name__ == '__main__':
     main()
